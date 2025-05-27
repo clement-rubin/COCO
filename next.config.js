c72b9a27dd555739337b7ad34c8243e30727799e
clement-rubin <144238940+clement-rubin@users.noreply.github.com>
2025-05-27 20:16:45 +0200

    jsp

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  swcMinify: true,
+  poweredByHeader: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 }
 
 module.exports = nextConfig
